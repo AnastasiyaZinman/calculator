da782c95b23a3b3cc793cccf97fbfe24d6625b38
AGCH <anastasiyache@gmail.com>
2018-12-02 16:44:24 +0300

    calculator grid

--- src/App.js
+++ src/App.js
@@ -6,20 +6,30 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="calculator">
+       <div className="screen"></div>
+       <div className="calculator-buttons">
+          <button className="simple-btn">C</button>
+          <button className="simple-btn">+/-</button>
+          <button className="simple-btn">%</button>
+          <button className="operation-btn">/</button>
+          <button className="simple-btn">7</button>
+          <button className="simple-btn">8</button>
+          <button className="simple-btn">9</button>
+          <button className="operation-btn">*</button>
+          <button className="simple-btn">4</button>
+          <button className="simple-btn">5</button>
+          <button className="simple-btn">6</button>
+          <button className="operation-btn">-</button>
+          <button className="simple-btn">1</button>
+          <button className="simple-btn">2</button>
+          <button className="simple-btn">3</button>
+          <button className="operation-btn">+</button>
+          <button className="simple-btn is-zero">0</button>
+          <button className="simple-btn">.</button>
+          <button className="operation-btn">=</button>
+          </div>
+       </div>
       </div>
     );
   }
